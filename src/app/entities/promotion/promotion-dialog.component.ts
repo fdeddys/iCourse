0a593b443f70b1d519bee8c50a2fdce9ab90af50
Stevian <stevians@gmail.com>
2018-08-27 18:42:00 +0700

    fix error dikit

--- src/app/entities/promotion/promotion-dialog.component.ts
+++ src/app/entities/promotion/promotion-dialog.component.ts
@@ -158,7 +158,9 @@ export class PromotionDialogComponent implements OnInit {
             this.memberCtrl.setValue(this.data.rowData.onBehalfMember);
             this.billTypeCtrl.setValue(this.data.rowData.applyToType);
             this.billCompanyCtrl.setValue(this.data.rowData.applyToCompany);
-            this.promotion.applyToProductId = this.data.rowData.applyToProduct.id;
+            if (this.data.rowData.applyToProduct !== null) {
+                this.promotion.applyToProductId = this.data.rowData.applyToProduct.id;
+            }
             this.billTypeCtrl.enable();
             this.billCompanyCtrl.enable();
         }
